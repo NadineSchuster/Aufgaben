5809909ff556ca5f54ead00f82ce04ecb44aadc7
NadineSchuster <samdee004@gmail.com>
2021-06-07 01:40:42 +0200

    add design elements

--- ToDoListe/app.js
+++ ToDoListe/app.js
@@ -9,21 +9,32 @@ let addTodoItem = function (event) {
 
   let liElem = document.createElement("li");
   liElem.classList.add("listItem");
-  liElem.textContent = inputField.value;
+
+  let p5 = document.createElement("p5");
+  p5.textContent = inputField.value;
+
+  // liElem.textContent = inputField.value;
 
   let deleteBtn = document.createElement("button");
   deleteBtn.textContent = "X";
   deleteBtn.addEventListener("click", deleteListItem);
 
+  let container = document.createElement("div");
+  container.classList.add("itemContainer");
+
   let checkBox = document.createElement("input");
   checkBox.setAttribute("type", "checkbox");
   checkBox.addEventListener("change", checkListItem);
 
-  liElem.appendChild(checkBox);
-  liElem.appendChild(deleteBtn);
+  container.appendChild(checkBox);
+  container.appendChild(p5);
+  container.appendChild(deleteBtn);
+
+  liElem.appendChild(container);
+  // liElem.appendChild(checkBox);
   todoList.appendChild(liElem);
 
-  inputField.value = "";
+  // inputField.value = "";
 };
 
 let deleteListItem = function (event) {
@@ -33,13 +44,16 @@ let deleteListItem = function (event) {
 
 let checkListItem = function (event) {
   let listItem = event.target.parentElement;
+  let textItem = event.target.nextElementSibling;
 
   if (event.target.checked) {
     console.log("entered if");
     listItem.classList.add("checkBox");
+    textItem.classList.add("textItem");
   } else {
     console.log("entered else");
     listItem.classList.remove("checkBox");
+    textItem.classList.remove("textItem");
   }
 };
 
